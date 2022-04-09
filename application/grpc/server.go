a2c97da280f3e4b8e9d0978b11a9acd4e52c7598
romuloBenjamin <romulo.b.franco@hotmail.com>
2022-04-09 20:59:08 +0000

    main...6

--- application/grpc/server.go
+++ application/grpc/server.go
@@ -6,21 +6,30 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
+	"github.com/romuloBenjamin/codepix-go/application/grpc/pb"
+	"github.com/romuloBenjamin/codepix-go/application/usecase"
+	"github.com/romuloBenjamin/codepix-go/infraestructure/repository"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func StartGrpcServer(database *gorm.DB, port int) {
 	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
 
-	address := fmt.Sprintf("0.0.0.0:#{port}")
-	listner, err := net.Listen("tcp", address)
+	Pixrepository := repository.PixKeyRepositoryDb{Db: database}
+	PixUseCase := usecase.PixUseCase{PixKeyRepository: Pixrepository}
+	pixGrpcService := NewPixGrpcService(PixUseCase)
+	pb.RegisterPixServiceServer(grpcServer, pixGrpcService)
+
+	address := fmt.Sprintf("0.0.0.0:%d", port)
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatal("cannot start")
+		log.Fatal("connot start grpc server", err)
 	}
-
-	log.Printf("gRPC start port %d", port)
-	err = grpcServer.Serve(listner)
+	log.Printf("grpx has been started on port %d", port)
+	err = grpcServer.Serve(listener)
 	if err != nil {
-		log.Fatal("not start grpc", err)
+		log.Fatal("connot start grpc server", err)
 	}
 }
