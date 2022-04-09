54ee8f5c17646cf5de9254558cddd490b8147f58
romuloBenjamin <romulo.b.franco@hotmail.com>
2022-04-09 20:34:05 +0000

    main...2

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
+	"github.com/romuloBenjamin/codepix-go/application/grpc"
 	"github.com/romuloBenjamin/codepix-go/infraestructure/db"
 )
 
@@ -11,4 +12,5 @@ var database *gorm.DB
 
 func main() {
 	database = db.ConnectDB(os.Getenv("env"))
+	grpc.StartGrpcServer(database, 50051)
 }
